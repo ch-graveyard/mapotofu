847bb8375ef46c25838cd667150175d3864c94b0
Chua Hou <extremelyspicystudios@gmail.com>
2019-07-18 21:52:23 +0800

    Add reset button for debugging purposes

---
+++ Assets/Scripts/DebugReset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugReset : MonoBehaviour
+{
+    /// <summary>
+    /// Reloads current scene; for debugging purposes.
+    /// </summary>
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
