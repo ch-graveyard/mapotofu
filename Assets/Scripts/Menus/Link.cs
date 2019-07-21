ddd6b3f64ae2aa0f28bbf844fd705be9d28027c1
Chua Hou <extremelyspicystudios@gmail.com>
2019-07-21 15:46:27 +0800

    Add about page

---
+++ Assets/Scripts/Menus/Link.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace CH.MapoTofu.UI
+{
+
+    public class Link : MonoBehaviour
+    {
+
+        /// <summary>
+        /// URL to open when button is clicked.
+        /// </summary>
+        public string url;
+
+        public void ButtonClicked()
+        {
+            Application.OpenURL(url);
+        }
+
+    }
+
+}
