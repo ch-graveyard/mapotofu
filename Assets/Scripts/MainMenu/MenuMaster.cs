b4457cabf705df9679d7577426c527ca955ea54a
Chua Hou <extremelyspicystudios@gmail.com>
2019-07-21 13:29:58 +0800

    Add main menu with yet to be implemented options

---
+++ Assets/Scripts/MainMenu/MenuMaster.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CH.MapoTofu.UI
+{
+
+    public class MenuMaster : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Handles play button clicked.
+        /// </summary>
+        public void PlayButtonClicked()
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+
+        /// <summary>
+        /// Handles custom game button clicked.
+        /// </summary>
+        public void CustomButtonClicked()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Handles about button clicked.
+        /// </summary>
+        public void AboutButtonClicked()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
